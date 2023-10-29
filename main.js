f3263ad2a6ecaa5f4b01e19aa55dec861f880892
Sandin-M <>
2023-10-29 15:29:15 +0000

    Add functionality for choice buttons to play sound when clicked

--- main.js
+++ main.js
@@ -16,10 +16,47 @@ function deleteHideClass(page) {
 function doSomething() {
     let selector = document.getElementById("difficulty").value;
     if (selector == "Simple") {
-        deleteHideClass('Choice1')
+        deleteHideClass('Choice1');
     } else if (selector == "Advanced") {
-        deleteHideClass('Choice2')
+        deleteHideClass('Choice2');
     } else if (selector == "Pro") {
-        deleteHideClass('Choice3')
+        deleteHideClass('Choice3');
     }
 }
+
+
+function playFirstNote(){
+    
+    
+}
+
+
+function playNote(note) {
+
+    const allNotes = {
+        "A": new Audio("assets/Sounds/A.wav"),
+        "A#": new Audio("assets/Sounds/A#.wav"),
+        "B": new Audio("assets/Sounds/B.wav"),
+        "C": new Audio("assets/Sounds/C.wav"),
+        "C#": new Audio("assets/Sounds/C#.wav"),
+        "D": new Audio("assets/Sounds/D.wav"),
+        "D#": new Audio("assets/Sounds/D#.wav"),
+        "E": new Audio("assets/Sounds/E.wav"),
+        "F": new Audio("assets/Sounds/F.wav"),
+        "F#": new Audio("assets/Sounds/F#.wav"),
+        "G": new Audio("assets/Sounds/G.wav"),
+        "G#": new Audio("assets/Sounds/G#.wav"),
+
+    };
+
+    for (const key in allNotes) {
+        if (key === note) {
+            allNotes[key].play();
+        }
+    }
+
+
+}
+
+
+
